c03a14b1576ff9e37f7bc8ea27b2a97398093873
Raph221001 <c18359123@tudublin.ie>
2021-12-11 14:03:57 +0000

    Adding terrain

---
+++ Project-2021-22/Assets/AddTerrain.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddTerrain : MonoBehaviour
+{
+    int heightscale = 5;
+    float detailScale = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mesh mesh = this.GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+        for (int v = 0; v < vertices.Length; v++)
+        {
+            vertices[v].y = Mathf.PerlinNoise((vertices[v].x + this.transform.position.x)/detailScale, 
+            (vertices[v].z + this.transform.position.z)/detailScale)*heightscale;
+        }
+
+        mesh.vertices = vertices;
+    mesh.RecalculateBounds();
+    mesh.RecalculateNormals();
+    this.gameObject.AddComponent<MeshCollider>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
