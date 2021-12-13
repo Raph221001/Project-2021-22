98f5ca6e0ce48bd832e4734ed481d58bc61bf82d
Raph221001 <c18359123@tudublin.ie>
2021-12-13 15:22:45 +0000

    Added jump button

--- Project-2021-22/Assets/Scripts/PlayerMovement.cs
+++ Project-2021-22/Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed = 12f;
     public float gravity = -9.81f;
+    public float jumpHeight = 3f;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
@@ -37,6 +38,11 @@ public class PlayerMovement : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && controller.isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity); 
+        }
 
         velocity.y += gravity * Time.deltaTime;
 
