cad393eb67d4ff0078f1ce8a3f068d701be3d053
Raph221001 <c18359123@tudublin.ie>
2021-12-13 12:47:48 +0000

    Added Day & Night Loop

---
+++ Project-2021-22/Assets/Scripts/LightingPreset.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "Lighting Preset", menuName = "Scriptables/Lighting Preset", order = 1)]
+public class LightingPreset : ScriptableObject
+{
+    public Gradient AmbientColor;
+    public Gradient DirectionalColor;
+    public Gradient FogColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
