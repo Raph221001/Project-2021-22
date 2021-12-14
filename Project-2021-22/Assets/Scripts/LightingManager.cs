323af53a0d7220552fe84680c3d7b9f702152365
Raph221001 <c18359123@tudublin.ie>
2021-12-14 23:47:18 +0000

    Added comments

--- Project-2021-22/Assets/Scripts/LightingManager.cs
+++ Project-2021-22/Assets/Scripts/LightingManager.cs
@@ -8,15 +8,18 @@ public class LightingManager : MonoBehaviour
     [SerializeField] private Light DirectionalLight;
     [SerializeField] private LightingPreset Preset;
 
-    [SerializeField, Range(0, 25)] private float TimeOfDay;
+    [SerializeField, Range(0, 35)] private float TimeOfDay;
     
     private void Update()
     {
+        //Checks if preset is assigned
         if(Preset == null)
             return;
         
         if(Application.isPlaying)
         {
+            //Time will be updated and then lighting
+            //TimeOfDay is equal to 35 as range is set from 0 - 35 
             TimeOfDay += Time.deltaTime;
             TimeOfDay %= 35;
             UpdateLighting(TimeOfDay / 35f);
@@ -24,13 +27,17 @@ public class LightingManager : MonoBehaviour
     }
     private void UpdateLighting(float timePercent)
     {
+        //Evaluates all of the different gradients in preset script
         RenderSettings.ambientLight = Preset.AmbientColor.Evaluate(timePercent);
         RenderSettings.fogColor = Preset.FogColor.Evaluate(timePercent);
 
+        //First checks if Directional is set
         if (DirectionalLight != null)
         {
+            //Changes Directional Light colour
             DirectionalLight.color = Preset.DirectionalColor.Evaluate(timePercent);
 
+            //Changes rotation
             DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, 170f, 0));
         }
     }
--- Project-2021-22/Assets/Scripts/LightingPreset.cs
+++ Project-2021-22/Assets/Scripts/LightingPreset.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
+//Enables an instance of the class to be created as a file in Unity
 [CreateAssetMenu(fileName = "Lighting Preset", menuName = "Scriptables/Lighting Preset", order = 1)]
 
 public class LightingPreset : ScriptableObject
