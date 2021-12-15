2b2c26559de40e4d8ce395c09a5f784de8594ccf
Raph221001 <c18359123@tudublin.ie>
2021-12-15 00:33:04 +0000

    Added comments

--- Project-2021-22/Assets/Scripts/MouseLook.cs
+++ Project-2021-22/Assets/Scripts/MouseLook.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-
+    //Controls the speed of mouse
     public float mouseSensitivity = 100f;
 
     public Transform playerBody;
@@ -14,20 +14,24 @@ public class MouseLook : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Locks the cursor onto the centre of screen
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Contains input for mouse movement
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+        //Clamp prevents player from over rotating and looking beyond the set 90 degrees
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
 
+        //Enables player to move relative to the mouse movement
         playerBody.Rotate(Vector3.up * mouseX);
     }
 }
